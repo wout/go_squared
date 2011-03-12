49cfa0738dc7bb5220b86e66eb954b94c77f377f
wout fierens <wout@boysabroad.com>
2011-03-12 14:54:46 +0000

    initial commit

---
+++ lib/go_squared.rb
@@ -0,0 +1,19 @@
+require 'net/http'
+require 'active_support/core_ext/hash/conversions'
+require 'go_squared/core_extensions'
+
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+class GoSquared
+  
+  def initialize api_key, site_id
+    @api_key = api_key
+    @site_id = site_id
+  end
+  
+  def trends
+    GoSquared::Trends.new(@api_key, @site_id)
+  end
+  
+end
